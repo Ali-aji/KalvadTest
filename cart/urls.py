b68ceef15f1e7acd428b721d45c759fa31a32d6f
Ali Aji <ali.a.aji108@gmail.com>
2023-05-18 22:02:59 +0400

    create base and cart tempalates, add views and urls.

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name="base"),
+    path('cart/', views.cart, name="cart"),
+]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Cart
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'base.html', context)
+
+
+def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        cart, created = Cart.objects.get_or_create(customer=customer, complete=False)
+        items = cart.cartitem_set.all()
+    else:
+        items = []
+        cart = {'get_cart_total':0, 'get_cart_items':0}
+
+
+    context = {'items': items, 'cart': cart}
+    return render(request, 'cart.html', context)

737ff2e95e01c7dd8b180bf1b9c09a783d121182
Ali Aji <ali.a.aji108@gmail.com>
2023-05-18 21:53:04 +0400

    update CartItem and Cart models.

--- cart/models/cart.py
+++ cart/models/cart.py
@@ -9,3 +9,15 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.get_total for item in cartitems])
+        return total 
+
+    @property
+    def get_cart_items(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.quantity for item in cartitems])
+        return total 
--- cart/models/cartItem.py
+++ cart/models/cartItem.py
@@ -7,6 +7,12 @@ class CartItem(models.Model):
 
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total

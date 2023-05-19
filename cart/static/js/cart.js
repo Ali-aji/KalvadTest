0ef3c7819f82731ba668bc05113591a5169b93fe
Ali Aji <ali.a.aji108@gmail.com>
2023-05-19 21:06:07 +0400

    adapt templates, create javascript code to update cart quantity and total price.

--- cart/static/js/cart.js
+++ cart/static/js/cart.js
@@ -1,38 +1,49 @@
-var updateButtons = document.getElementsByClassName('update-cart')
+var updateButtons = document.getElementsByClassName('update-cart');
+let $allLinePrice = $(".line-price");
+let $totalPrice = $(".total-price");
+let totalPrice = 0;
+$allLinePrice.each(function (){
+    totalPrice += Number($(this).text());
+})
+$totalPrice.text(Number(totalPrice))
 
 for(i=0; i < updateButtons.length; i++){
     updateButtons[i].addEventListener('click', function (){
-        var productId = this.dataset.product
-        var action = this.dataset.action
+        let productId = this.dataset.product;
+        let action = this.dataset.action;
 
-        if (user == 'AnonymousUser'){
-			//  default items
-            console.log("AnonymousUser")
-		}else{
-			updateUserCart(productId, action)
-		}
+        // update product quantity
+        let $quantityVal = $(this).parent().prev();
+        let quantityVal = Number($quantityVal.text())
+        if (action==='add') quantityVal += 1;
+        if (action==='remove') quantityVal -= 1;
+        if (quantityVal < 1) quantityVal = 1;
+        $quantityVal.text(quantityVal);
+
+        // update product line price
+        let $price = $(this).parent().parent().next();
+        let $linePrice = $(this).parent().parent().next().next();
+        let price = Number($price.text())
+        let newLinePrice = price * quantityVal
+        $linePrice.text(newLinePrice)
+        $linePrice.data({"linePrice": newLinePrice})
+        $linePrice.addClass("line-price")
+
+        // update total price
+        let $allLinePrice = $(".line-price");
+        totalPrice = 0
+        $allLinePrice.each(function (){
+            totalPrice += Number($(this).text())
+        })
+        $totalPrice.text(totalPrice)
+
+        // Update cart items form
+        let formItemId = $(this).parent().attr("id");
+        let $formItemQuantity = $(`form>div>input#id_items-${formItemId}-quantity`);
+        let $formItemId = $(`form>div>input#id_items-${formItemId}-id`);
+        $formItemQuantity.val(quantityVal);
+
 
     })
 
-}
-
-function updateUserCart(productId, action){
-
-    var url = '/update_item/'
-
-    fetch(url, {
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
-        }, 
-        body:JSON.stringify({'productId':productId, 'action':action})
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        location.reload()
-        console.log(data)
-    });
 }

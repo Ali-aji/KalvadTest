ed50a9a1ca41856b49904659bbbfadc4fcd5fb8c
Ali Aji <ali.a.aji108@gmail.com>
2023-05-22 17:33:59 +0400

    apdate html and js files.

--- cart/static/js/cart.js
+++ cart/static/js/cart.js
@@ -1,11 +1,40 @@
-var updateButtons = document.getElementsByClassName('update-cart');
+$(document).ready(function (){var updateButtons = document.getElementsByClassName('update-cart');
 let $allLinePrice = $(".line-price");
 let $totalPrice = $(".total-price");
 let totalPrice = 0;
+
+let formIds = []
 $allLinePrice.each(function (){
     totalPrice += Number($(this).text());
+    let formItemId = $(this).attr("id");
+    formIds.push(formItemId);  
 })
+console.log(formIds);
 $totalPrice.text(Number(totalPrice))
+
+// id_items-0-product
+let $formItemIds = $("form>div>select[id$='-product']");
+let formIdExtractorRegex = /^id_items-(\d+)-quantity$/m;
+let hiddenFormIds = [];
+
+
+var match_ = -1;
+$formItemIds.each(function () {
+    let formItemId = $(this).attr("id");
+    let id = formItemId.split('-')[1]
+
+    if(formIds.indexOf($(this).val()) <0){
+        console.log(`form>div>input#id_items-${id}-quantity`, $(`form>div>input#id_items-${id}-quantity`));
+        let $formItemDelete = $(`form>div>input#id_items-${id}-DELETE`).prop( "checked", true ); //id_items-0-DELETE
+        // $formItemQuantity.val(0);
+    }
+    // console.log(formItemId, formIdExtractorRegex.exec(formItemId), formItemId.match(formIdExtractorRegex));
+    // match_ = formIdExtractorRegex.exec(formItemId)[1]
+    hiddenFormIds.push($(this).val());
+})
+console.log(hiddenFormIds);
+
+
 
 for(i=0; i < updateButtons.length; i++){
     updateButtons[i].addEventListener('click', function (){
@@ -47,3 +76,4 @@ for(i=0; i < updateButtons.length; i++){
     })
 
 }
+})

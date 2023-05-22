2f7f864b1333f355f8ce1f2d8044a4fa7697ff55
Ali Aji <ali.a.aji108@gmail.com>
2023-05-22 18:06:00 +0400

    clean cart.js file.

--- cart/static/js/cart.js
+++ cart/static/js/cart.js
@@ -6,39 +6,21 @@ let totalPrice = 0;
 let formIds = []
 $allLinePrice.each(function (){
     totalPrice += Number($(this).text());
-    let formItemId = $(this).attr("id");
-    formIds.push(formItemId);  
+    formIds.push($(this).attr("id"));  
 })
-console.log(formIds);
 $totalPrice.text(Number(totalPrice))
 
-// id_items-0-product
 let $formItemIds = $("form>div>select[id$='-product']");
-let formIdExtractorRegex = /^id_items-(\d+)-quantity$/m;
-let hiddenFormIds = [];
-
-
-var match_ = -1;
 $formItemIds.each(function () {
-    let formItemId = $(this).attr("id");
-    let id = formItemId.split('-')[1]
+    let id = $(this).attr("id").split('-')[1]
 
     if(formIds.indexOf($(this).val()) <0){
-        console.log(`form>div>input#id_items-${id}-quantity`, $(`form>div>input#id_items-${id}-quantity`));
-        let $formItemDelete = $(`form>div>input#id_items-${id}-DELETE`).prop( "checked", true ); //id_items-0-DELETE
-        // $formItemQuantity.val(0);
+        $(`form>div>input#id_items-${id}-DELETE`).prop( "checked", true );
     }
-    // console.log(formItemId, formIdExtractorRegex.exec(formItemId), formItemId.match(formIdExtractorRegex));
-    // match_ = formIdExtractorRegex.exec(formItemId)[1]
-    hiddenFormIds.push($(this).val());
 })
-console.log(hiddenFormIds);
-
-
 
 for(i=0; i < updateButtons.length; i++){
     updateButtons[i].addEventListener('click', function (){
-        let productId = this.dataset.product;
         let action = this.dataset.action;
 
         // update product quantity
@@ -46,7 +28,9 @@ for(i=0; i < updateButtons.length; i++){
         let quantityVal = Number($quantityVal.text())
         if (action==='add') quantityVal += 1;
         if (action==='remove') quantityVal -= 1;
-        if (quantityVal < 1) quantityVal = 1;
+        if (quantityVal < 1) quantityVal = 0;
+
+        
         $quantityVal.text(quantityVal);
 
         // update product line price
